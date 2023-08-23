0b2feaf0697589946dc5db159b2de95928be87af
Andrew Nelson <anelson2037@gmail.com>
2023-08-22 23:00:17 -0400

    Admin profile pages

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +21,9 @@ Route::prefix('admin')->middleware(['auth:admin', 'verified'])->group(function (
 });
 
 Route::prefix('admin')->middleware('auth:admin')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
 });
 
 require __DIR__.'/auth.php';
