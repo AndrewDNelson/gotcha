bbcac6418fa0039242da5b098fb42cac6bdf8ae3
Andrew Nelson <anelson2037@gmail.com>
2023-09-12 14:43:50 -0700

    Fixed verify admin route name

--- app/Notifications/admin/VerifyEmail.php
+++ app/Notifications/admin/VerifyEmail.php
@@ -84,7 +84,7 @@ class VerifyEmail extends Notification
         }
 
         return URL::temporarySignedRoute(
-            'verification.verify',
+            'admin.verification.verify',
             Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
             [
                 'id' => $notifiable->getKey(),
