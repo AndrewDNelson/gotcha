662620cc998a8fb6e1f3031428d826b88673e798
Andrew Nelson <anelson2037@gmail.com>
2023-11-22 23:32:18 -0800

    App layout using new nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,69 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+<x-shared-layout>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            <x-navigation.admin :pages="[
+                    ['route' => 'admin.dashboard', 'text' => 'Dashboard'],
+                    ['route' => 'dashboard', 'text' => 'Hello'],
+                    ['route' => 'dashboard', 'text' => 'Bye'],
+                ]">
+                <x-slot:right>
+                    <!-- Settings Dropdown -->
+                    <div class="hidden md:flex md:items-center">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('admin.profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('admin.logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('admin.logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </x-slot:right>
+
+                <x-slot:bottom>
+                    <div class="p-6 flex flex-col gap-4">
+                        <div>
+                            <div class="text-black">{{ Auth::user()->name }}</div>
+                            <div class="text-slate-400">{{ Auth::user()->email }}</div>
+                        </div>
+                        <a href="{{ route('profile.edit')}}" class="px-5 py-2 h-12 transition group gap-2 flex items-center justify-center rounded-md bg-transparent text-base font-semibold text-black shadow-sm border-slate-200 border-2  hover:border-slate-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                            Profile
+                        </a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+        
+                            <button class="px-5 py-2 h-12 w-full group gap-2 flex items-center justify-center rounded-md bg-black text-base font-semibold text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                                    type="submit" :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </div>
+                </x-slot:bottom>
+            </x-navigation>
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -33,4 +80,4 @@
             </main>
         </div>
     </body>
-</html>
+</x-shared-layout>
